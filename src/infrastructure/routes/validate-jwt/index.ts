bab692c4ec4a55e31cde7b15355de126f08c7b37
janovoas <javiernovoa.s@gmail.com>
2022-06-25 14:01:33 -0400

    changed name to validate payload variable

--- src/infrastructure/routes/validate-jwt/index.ts
+++ src/infrastructure/routes/validate-jwt/index.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import { Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
-import Joi from 'joi'
 import fs from 'fs'
 
 export type ValidateJWTRouteFnType = (router: Router) => Router
@@ -10,13 +9,13 @@ export const validateJWTHandler = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const { accToken } = req.body
+  const { token } = req.body
   const vaultSecretsPath = process.env.VAULT_SECRET_FILE_PATH
 
   const publicKey = fs.readFileSync(`${vaultSecretsPath}pubk.pem`)
 
   try {
-    const decoded = await jwt.verify(accToken, publicKey, {
+    const decoded = await jwt.verify(token, publicKey, {
       algorithms: ['RS256'],
     })
     return res.status(200).send(decoded)
